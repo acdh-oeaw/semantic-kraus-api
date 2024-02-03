a234745e9872ef7fcad95d8cf750892d7eb3c5af
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2024-02-03 10:48:20 +0100

    fix: setts to 4 workers

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 if [[ -z "${DEVELOP}" ]]; then
     echo "starting gunicorn"
-    gunicorn semantic_kraus_api.main:app -w 1 --timeout=200 --threads=4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000
+    gunicorn semantic_kraus_api.main:app -w 4 --timeout=200 --threads=4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000
 fi
